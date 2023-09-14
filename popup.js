a640553c3eb5af21a97db0d02cb3c113a860ff07
Per Sondell <c19psl@cs.umu.se>
2023-09-14 13:38:26 +0200

    init of project

---
+++ popup.js
@@ -0,0 +1,6 @@
+// popup.js
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("helloButton").addEventListener("click", function () {
+      console.log("hello");
+    });
+  });
